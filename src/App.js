e31c7c9d67ff883c15e8e32c1ac3c607fd262a1e
mdemon420 <mdeamon4@gmail.com>
2021-08-11 21:21:50 +0600

    2nd Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
-
+import  News from './Components/News/News.js';
 function App() {
   return (
     <div >
       <h2>React Bootstrap</h2>
+      <News></News>
     </div>
   );
 }
